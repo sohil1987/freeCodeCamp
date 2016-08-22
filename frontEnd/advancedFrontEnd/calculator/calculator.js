5b90003a11eecf1cbc7bc2ff798fe5fa1251cf37
brusbilis <brusbilis@gmail.com>
2016-08-22 22:15:16 +0200

    calculator

--- frontEnd/advancedFrontEnd/calculator/calculator.js
+++ frontEnd/advancedFrontEnd/calculator/calculator.js
@@ -2,6 +2,10 @@
   'use strict';
   /* code here */
 
+  var result = 0;
+  var log = 0;
+  var operation = '';
+
   function init () {
     console.log('Inicio');
     var clickType = document.getElementsByClassName('card');
@@ -12,9 +16,41 @@
   }
 
   function clickCal (ev) {
-    console.log(ev.target.innerText);
     var action = ev.target.innerText;
-    document.getElementById('display').innerText = action;
+    var prev = document.getElementById('display').innerText;
+    if (action === '=') {
+      printResult(result, log);
+    } else if (action === 'AC') {
+      log = 0;
+      result = 0;
+      operation = '';
+      printResult(0, 0);
+    } else if (action === 'CE') {
+      result = 0;
+      printResult(0, log);
+    } else if (action === '.') {
+      log += log;
+      printResult(prev, log);
+    } else if (Number.isNaN(parseInt(action))) {
+      if (operation === '+') {
+        result += result;
+      }
+      if (operation === '-') {}
+      if (operation === '&#247') {}
+      if (operation === '&#215') {}
+      operation = action;
+      log += log;
+      printResult(prev, log);
+    } else {
+      console.log('NUMERO', action);
+      log += log;
+      printResult(result, log);
+    }
+  }
+
+  function printResult (display, minidisplay) {
+    document.getElementById('display').innerText = display;
+    document.getElementById('minidisplay').innerText = minidisplay;
   }
 
   addEventListener('load', init);
